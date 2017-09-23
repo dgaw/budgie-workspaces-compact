1e0b592e3bfa4aa74d035fc6065c7545c14ec9bf
Damian Gaweda <damian.gaweda@gmail.com>
2017-09-23 09:19:44 +0200

    Installer tweaks

--- install.sh
+++ install.sh
@@ -2,14 +2,17 @@
 
 PLUGIN_DIR="/usr/lib/budgie-desktop/plugins" 
 
-if [ -d "$PLUGIN_DIR" ]; then
-    echo "OK"
-else
+if [ ! -d "$PLUGIN_DIR" ]
+then
     echo "The Budgie plugin directory does not exist: $PLUGIN_DIR"
     exit 1
 fi
 
-echo "Installing workspaces-compact-applet to $PLUGIN_DIR"
-sudo cp -r ./src/workspaces-compact-applet "${PLUGIN_DIR}/"
-budgie-panel --replace &
-echo "Done. You should be able to add the applet to your panel now."
+echo "Installing Compact Workspace Switcher to $PLUGIN_DIR"
+if sudo cp -r ./src/workspaces-compact-applet "${PLUGIN_DIR}/" && budgie-panel --replace &
+then
+    echo "Done. You should be able to add the applet to your panel now." 
+else
+    echo "Install failed"
+    exit 1
+fi
